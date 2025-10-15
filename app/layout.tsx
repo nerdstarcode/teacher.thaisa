d0ffa48a70b38454e2771cd07017ff48c63da080
Sthiven <sthivendev@gmail.com>
2025-10-15 20:54:04 -0300

    :tada: add hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -62,6 +63,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Hotjar Tracking Script */}
+      <Script id="hotjar-init" strategy="afterInteractive">
+        {`
+            (function(h,o,t,j,a,r){
+              h.hj = h.hj || function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6548276,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
